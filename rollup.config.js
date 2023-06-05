351e4a93a3ee9ee37e75d6d28adee526f91958cc
Krish <kanarang@gmail.com>
2023-06-04 23:09:59 -0700

    update webllm

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ export default {
             exports: 'named',
             format: 'es',
             sourcemap: true,
-            // globals: { 'ws': 'ws' }
+            globals: { 'ws': 'ws' }
         }
     ],
     plugins: [
