717cf32c6c3850a981eb4fa4e848394b3acd232b
narangkay <kanarang@gmail.com>
2023-06-13 22:21:01 -0700

    remove lib

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { ModelRecord, AppConfig } from "./config";
-export { InitProgressCallback, InitProgressReport, ChatOptions, ChatInterface } from "./types";
-export { ChatModule, } from "./chat_module";
-export { ChatWorkerHandler, ChatWorkerClient } from "./web_worker";
-//# sourceMappingURL=index.d.ts.map
