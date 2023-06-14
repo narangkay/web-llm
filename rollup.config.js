631d709340b5fab443ea913e8f7df799e6a94354
narangkay <kanarang@gmail.com>
2023-06-13 21:23:58 -0700

    use newer tvmjs

--- rollup.config.js
+++ rollup.config.js
@@ -14,6 +14,7 @@ export default {
             globals: { 'ws': 'ws' }
         }
     ],
+    inlineDynamicImports: true,
     plugins: [
         ignore(["fs", "path", "crypto", "module"]),
         nodeResolve({ browser: true }),
